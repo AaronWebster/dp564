dc327334ec3bd1c786509641971feb0d41a267dc
Aaron Webster <webstera@google.com>
2023-02-15 09:49:02 -0800

    Add some chatGPT documentation.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Dolby DP564 Arduino Remote Control
 
 Uses an Arduino ESP8266.
+
+Here are the MAYBE packet types defined by the DP564 binary protocol:
+
+
--- volume.ino
+++ volume.ino
@@ -2,9 +2,10 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+
 #include "ESPRotary.h";
 
-constexpr char kApSsid[] = "";
+constexpr char kApSsid[] = "FBI Party Van";
 constexpr char kApPassword[] = "";
 
 IPAddress kHost(192, 168, 4, 91);
@@ -17,10 +18,42 @@ constexpr int kKnobMinValue = 0x02;
 constexpr int kKnobMaxValue = 0xbf;
 constexpr int kKnobClicksPerStep = 4;
 
-ESPRotary knob = ESPRotary(kKnobPinClk, kKnobPinDt, kKnobClicksPerStep, kKnobMinValue, kKnobMaxValue, 0);
+ESPRotary knob = ESPRotary(kKnobPinClk, kKnobPinDt, kKnobClicksPerStep,
+                           kKnobMinValue, kKnobMaxValue, 0);
 int knob_position;
 
 WiFiClient client;
+
+// PROBABLY WRONG:
+//
+// 0x00: Heartbeat packet. This packet is used to maintain the connection
+// between the device and the controller. It typically contains no payload.
+//
+// 0x01: Handshake request packet. This packet is used to initiate a new session
+// with the device. The payload of the packet should contain the string "Dolby".
+//
+// 0x02: Handshake response packet. This packet is sent in response to a
+// handshake request packet, and confirms that the connection has been
+// established successfully. The payload of the packet should contain the string
+// "ACK ".
+//
+// 0x03: Handshake completion packet. This packet is sent to indicate that the
+// handshake process has completed successfully. It typically contains no
+// payload.
+//
+// 0x04: Heartbeat request packet. This packet is used to request a heartbeat
+// response from the device. It typically contains no payload.
+//
+// 0x05: Heartbeat response packet. This packet is sent in response to a
+// heartbeat request packet. It typically contains no payload.
+//
+// 0x0a: Control packet. This packet is used to send control commands to the
+// device, such as changing the volume or routing signals. The format of the
+// packet varies depending on the specific control command being sent.
+//
+// 0x0b: Control response packet. This packet is sent in response to a control
+// packet to acknowledge the command and provide any relevant status or
+// information.
 
 void setup() {
   Serial.begin(115200);
@@ -49,17 +82,30 @@ void loop() {
   knob.loop();
   while (client.available()) client.read();
 
+  // Is this really a header?
+  // Byte 1: 0x3a (start of packet marker)
+  // Byte 2: 0x0a (packet type - control packet)
+  // Byte 3-4: 0x000c (packet length, including header and payload)
+  // Byte 5: 0x00 (flags - none)
+  // Byte 6-16: 0x00 (reserved)
+
   if (!client.connected()) {
     if (client.connect(kHost, kPort)) {
       Serial.print("Connected to ");
       Serial.print(kHost);
       Serial.print(":");
       Serial.println(kPort);
+
+      // Byte 1: 0x00 (reserved)
+      // Byte 2: 0x00 (reserved)
+      // Byte 3: 0x00 (reserved)
+      // Byte 4: 0x05 (length of the payload)
+      // Byte 5: 0x03 (payload - handshake request)
       uint8_t hello1[] = {0x00, 0x00, 0x00, 0x05};
-      client.write(hello1, sizeof(hello1));
+      client.write(hello1, sizeof(hello1), 0x18);  // Set PSH=1, ACK=1
       client.flush();
       uint8_t hello2[] = {0x03};
-      client.write(hello2, sizeof(hello2));
+      client.write(hello2, sizeof(hello2), 0x18);  // Set PSH=1, ACK=1
       client.flush();
     } else {
       Serial.println("connection failed");
@@ -72,7 +118,15 @@ void loop() {
 
   if (knob_position != knob.getPosition()) {
     knob_position = knob.getPosition();
-    uint8_t command[] = {0x00, 0x00, 0x00, 0x0a, 0x02, 0x03, 0x12, 0x00, 0x00, knob_position};
+    // Start of new control command, 0x0a represents that the command has 10
+    // bytes total.
+    uint8_t control[] = {0x00, 0x00, 0x00, 0x0a};
+    // 6-byte payload
+    // Byte 1-2: 0x0205 (device ID - fixed value)
+    // Byte 3-4: 0x1100 (control command ID - fixed value)
+    // Byte 5: 0x00 (parameter count - always zero)
+    // Byte 6: volume setting (0-255)
+    uint8_t value[] = {0x02, 0x03, 0x12, 0x00, 0x00, knob_position};
     client.write(command, sizeof(command));
     client.flush();
     while (client.available()) client.read();
